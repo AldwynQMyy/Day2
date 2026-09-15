9df8608a403910eba760a4585e7451e8ba803d05
AldwynQMyy <cwzhang0529@gmail.com>
2026-09-15 03:38:48 +0000

    Update_3

--- home.py
+++ home.py
@@ -3,5 +3,5 @@ from openai import OpenAI
 
 
 st.title("Day 2")
-st.header("How to step streamlit application?")
-st.write("Step 1. Create home.py file, using 'touch home.py' command in terminal")
+st.header("This includes the exercises for Day 2 of the OpenAI Streamlit course.")
+st.write("Exercises are located in the `pages` folder. You can navigate to them using the sidebar on the left.")
